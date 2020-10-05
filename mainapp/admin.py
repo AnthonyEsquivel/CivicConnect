064c786c7d3e0739b06514e4e105ccbfea9d5cb5
wt2cv@virginia.edu <70534021+wt2cv@users.noreply.github.com>
2020-10-05 17:59:00 -0400

    Create template admin and linked URL

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Template
 
 # Register your models here.
+class TemplateAdmin(admin.ModelAdmin):
+    model = Template
+admin.site.register(Template)
