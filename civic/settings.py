9528acb0b1f4e1d9c595eee184398e0f131ce6c2
mehta-shreyas <61636558+mehta-shreyas@users.noreply.github.com>
2020-10-28 19:08:48 -0400

    Removed social_django

--- civic/settings.py
+++ civic/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
