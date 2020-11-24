a5a2efc49c4c66905cb205d2fbd4c6df806707d8
AnthonyEsquivel <45054464+AnthonyEsquivel@users.noreply.github.com>
2020-11-24 15:28:35 -0500

    Edits and test cases

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -24,6 +24,11 @@ class TemplateModelTests(TestCase):
         except IntegrityError:
             error = True
         self.assertTrue(error)
+    # does my template have no tags if I don't assign it any
+    def testTemplateNoTags(self):
+        a = Template(temp_name='ASPCA', temp_description='Help the Animals', temp_text='Dear Senator...')
+        a.save()
+        self.assertEquals(a.tags.all(),None)
 
 class User(TestCase):
     #Does MyUser address setup save to user
@@ -32,3 +37,25 @@ class User(TestCase):
          a.myuser = MyUser(address="1000 W Main Street", member_since=timezone.now())
          a.save()
          self.assertEquals(a.myUser.address, "1000 W Main Street")
+
+    # Do tags get added to user's issues correctly
+    def testUserIssues(self):
+        t = Tags(name='Test Tag',id=100)
+        t.save()
+        a = User(first_name="User")
+        a.myuser = MyUser(address="1000 W Main Street", member_since=timezone.now())
+        a.myuser.save()
+        a.save()
+        a.myuser.issues.add(t)
+        a.myuser.save()
+        if t in a.myuser.issues.all():
+            self.assertTrue
+        else:
+            self.assertFalse
+
+class Tags(TestCase):
+    # Does the tag creation work correctly
+    def testTagCreation(self):
+        tag = Tags(name='Test tag',id=100)
+        tag.save()
+        self.assertEquals(tag.name,'Test tag')
